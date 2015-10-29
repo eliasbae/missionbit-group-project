ebfa63891aa5cc278a4f3074733fa3b99f9fb234
Joe Williams <missionbit@mediumaquamarine-canvas.local>
2015-10-28 17:57:46 -0700

    i dont wanna b judged

--- Menu.js
+++ Menu.js
@@ -2,9 +2,12 @@ var Menu = {
     
     preload : function() {
         game.load.image('menu', '/assets/images/missionbit.jpg')
-    }
+    },
         create: function () { 
             this.add.sprite(0, 0, 'menu');
-    }
+    },
+startgame.function () {
+    this.state.startgame
+}
 
 };
