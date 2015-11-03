fb67fab314154eb43842b901160e5697a62d0b91
Joe Williams <missionbit@mediumaquamarine-canvas.local>
2015-11-02 17:51:51 -0800

    ADDED SOME BUTTONS N' SHIT
    YEEEEEEEEEEAAAAAAAAH

--- Options.js
+++ Options.js
@@ -1,14 +1,20 @@
 var Options = {
     
     preload : function() {
-        game.load.image('optionsTitle', '/assets/images/options.png')
-    },
+        game.load.image('optionsTitle', '/assets/images/options.jpg')
+        game.load.image('startTitle', '/assets/images/start.jpg')
+        game.load.image('returnTitle', '/assets/images/return.jpg')},
+    
     
     create: function () { 
         this.add.sprite(0, 0, 'optionsTitle');
+        this.add.button(0, 350, 'returnTitle', this.titleScreen, this);
     },
-    
+:// returnTitle:function(){
+          ://   game.state.save('titleScreen');
+    ://     }
     update: function() {
 
     }
 };
+ 
--- game.js
+++ game.js
@@ -1,11 +1,11 @@
 var Game = {
     
     preload : function() {
-        game.load.image('optionsTitle', '/assets/images/options.png')
+        game.load.image('optionsTitle', '/assets/images/start.jpg')
     },
     
     create: function () { 
-        this.add.sprite(0, 0, 'optionsTitle');
+        this.add.sprite(0, 0, 'startTitle');
     },
     
     update: function() {
