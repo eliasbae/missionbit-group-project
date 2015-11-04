044668882184c743aacd7d0a133233923a47b0e7
Ruellia Zhang <syncyes@gmail.com>
2015-11-04 13:19:36 -0800

    fixed interaction br screens

--- options.js
+++ options.js
@@ -1,11 +1,18 @@
 var Options = {
     
     preload : function() {
-        game.load.image('optionsTitle', '/assets/images/options.jpg')
+        game.load.image('optionsTitle', '/assets/images/options.jpg');
+        game.load.image('returnTitle', '/assets/images/return.jpg');
+        game.load.image('startTitle', '/assets/images/start.jpg');
     },
     
     create: function () { 
         this.add.sprite(0, 0, 'optionsTitle');
+        this.add.button(0, 350, 'returnTitle', this.titleScreen, this);
+    },
+    
+    titleScreen: function() {
+        game.state.start('TitleScreen');
     },
     
     update: function() {
--- titleScreen.js
+++ titleScreen.js
@@ -1,12 +1,17 @@
 var TitleScreen = {
     
     preload : function() {
-        game.load.image('titleImage', '/assets/images/missionbit.png')
+        game.load.image('optionsTitle', '/assets/images/options.jpg');
+        game.load.image('startTitle', '/assets/images/start.jpg');
+        game.load.image('titleImage', '/assets/images/missionbit.png');
+        game.load.image('titleText', '/assets/images/title.jpg');
     },
     
-    create: function () { 
-        //button at position (0,0) that looks like titleImage and, when clicked, executes the options function
-        this.add.button(0, 0, 'titleImage', this.options, this);
+    create: function () {
+        this.add.image(200, 0, 'titleImage');
+        this.add.image(175, 250, 'titleText');
+        this.add.button(350, 350, 'optionsTitle', this.options, this);
+        this.add.button(0, 350, 'startTitle', this.startGame, this);
     },
     
     options: function () {
