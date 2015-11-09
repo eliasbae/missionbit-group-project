8b9e7b64847818dacbfaa66ec2fcf04b1db488d0
syncyes <ruelliazhang@Ruellias-MacBook-Air.local>
2015-11-09 12:40:54 -0800

    actual manual merge

--- titleScreen.js
+++ titleScreen.js
@@ -3,15 +3,19 @@ var TitleScreen = {
     preload : function() {
         game.load.image('optionsTitle', '/assets/images/options.jpg');
         game.load.image('startTitle', '/assets/images/start.jpg');
-        game.load.image('titleImage', '/assets/images/missionbit.png');
+        game.load.image('titleImage', '/assets/images/missionbit 2.png');
         game.load.image('titleText', '/assets/images/title.jpg');
     },
     
     create: function () {
-        this.add.image(200, 0, 'titleImage');
-        this.add.image(175, 250, 'titleText');
-        this.add.button(350, 350, 'optionsTitle', this.options, this);
-        this.add.button(0, 350, 'startTitle', this.startGame, this);
+        this.add.image(0, 0, 'titleImage');
+        this.add.image(576, 0, 'titleImage');
+        this.add.image(576, 416, 'titleImage');
+        this.add.image(0, 416, 'titleImage');
+        this.add.image(200, 0, 'titleText');
+        this.add.button(200, 250, 'optionsTitle', this.options, this);
+        this.add.button(200, 150, 'startTitle', this.startGame, this);
+        game.stage.backgroundColor = '#00FF00';
     },
     
     options: function () {
