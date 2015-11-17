74d0313ddb37fef8ce4bf412df58bb05b2c3bf71
GabeAradon <gabearzadon@gmail.com>
2015-11-16 17:53:15 -0800

    aesthetically nicer

--- options.js
+++ options.js
@@ -1,13 +1,13 @@
 var Options = {
     
     preload : function() {
-        game.load.image('optionsTitle', '/assets/images/options.jpg');
-        game.load.image('returnTitle', '/assets/images/return.jpg');
+        game.load.image('optionsTitle', '/assets/images/imageedit_1_5623684860.png');
+        game.load.image('returnTitle', '/assets/images/rsz_backt.png');
         game.load.image('startTitle', '/assets/images/start.jpg');
     },
     
     create: function () { 
-        this.add.sprite(0, 0, 'optionsTitle');
+        this.add.sprite(150, 0, 'optionsTitle');
         this.add.button(0, 350, 'returnTitle', this.titleScreen, this);
     },
     
--- titleScreen.js
+++ titleScreen.js
@@ -1,10 +1,11 @@
 var TitleScreen = {
     
     preload : function() {
-        game.load.image('optionsTitle', '/assets/images/options.jpg');
-        game.load.image('startTitle', '/assets/images/start.jpg');
+        game.load.image('optionsTitle', '/assets/images/rsz_1options.png');
+        game.load.image('startTitle', '/assets/images/rsz_1rsz_play.png');
         game.load.image('titleImage', '/assets/images/missionbit 2.png');
         game.load.image('titleText', '/assets/images/title.jpg');
+        game.load.image('titleHelp', '/assets/images/rsz_help.png');
     },
     
     create: function () {
@@ -12,10 +13,11 @@ var TitleScreen = {
         this.add.image(576, 0, 'titleImage');
         this.add.image(576, 416, 'titleImage');
         this.add.image(0, 416, 'titleImage');
-        this.add.image(200, 0, 'titleText');
-        this.add.button(200, 250, 'optionsTitle', this.options, this);
-        this.add.button(200, 150, 'startTitle', this.startGame, this);
-        game.stage.backgroundColor = '#00FF00';
+        this.add.image(190, 0, 'titleText');
+        this.add.button(290, 250, 'optionsTitle', this.options, this);
+        this.add.button(290, 150, 'startTitle', this.startGame, this);
+        this.add.button(290, 350, 'titleHelp', this.help, this);
+        game.stage.backgroundColor = '#66ffcc';
     },
     
     options: function () {
