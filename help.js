c49d4f1d202397c2c6b625f481c80dc217044c4a
GabeAradon <gabearzadon@gmail.com>
2015-12-02 17:50:21 -0800

    progress

--- help.js
+++ help.js
@@ -7,6 +7,15 @@ var Help = {
     create: function () {
       this.add.sprite(150, 0, 'helpTitle');
       this.add.button(0, 350, 'returnTitle', this.titleScreen, this);
+      var style = {font: '50px Sans Serif', fill:'#FFFFFF', align: 'center'};
+      var styleStep = {font: '20px Sans Serif', fill:'#FFFFFF', align: 'center'};
+      this.instructions = game.add.text(140,180, 'INSTRUCTIONS', style);
+      this.step1 = game.add.text(140,230, '1. Player 1 uses up and down arrow keys to move.', styleStep);
+      this.step2 = game.add.text(140,270, '2. Player 2 uses W and S keys to move.', styleStep);
+      this.step3 = game.add.text(140,310, '3. Use the paddle to block the ball from hitting your side.', styleStep);
+      this.step4 = game.add.text(140,350, '4. You get a point when it hits the opposite wall.', styleStep);
+      this.step5 = game.add.text(140,390, '5. Try to reach 30 points to win.', styleStep);
+      this.step6 = game.add.text(140,430, '6. HAVE FUN!', styleStep);
    },
     
     titleScreen: function() {
--- titleScreen.js
+++ titleScreen.js
@@ -1,11 +1,11 @@
 var TitleScreen = {
     
     preload : function() {
-        game.load.image('optionsTitle', '/assets/images/rsz_1options.png');
         game.load.image('startTitle', '/assets/images/rsz_1rsz_play.png');
         game.load.image('titleImage', '/assets/images/missionbit 2.png');
         game.load.image('titleText', '/assets/images/title.jpg');
         game.load.image('titleHelp', '/assets/images/rsz_help.png');
+        game.load.image('titleCredit', '/assets/images/credits.png');
     },
     
     create: function () {
@@ -14,15 +14,10 @@ var TitleScreen = {
         this.add.image(576, 416, 'titleImage');
         this.add.image(0, 416, 'titleImage');
         this.add.image(120, 0, 'titleText');
-        this.add.button(290, 250, 'optionsTitle', this.options, this);
         this.add.button(290, 150, 'startTitle', this.startGame, this);
-        this.add.button(290, 350, 'titleHelp', this.help, this);
+        this.add.button(290, 250, 'titleHelp', this.help, this);
+        this.add.button(290, 350, 'titleCredit', this.credit, this);
         game.stage.backgroundColor = '#66ffcc';
-    },
-    
-    options: function () {
-        //begins the Options state
-        this.state.start('Options');
     },
 
     startGame: function() {
@@ -31,5 +26,9 @@ var TitleScreen = {
     
     help: function() {
         this.state.start('Help');
+    },
+    
+    credit: function() {
+        this.state.start('Credits');
     },
 };
