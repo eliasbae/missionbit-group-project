e090c64e5497952c8dd0af7bab132d5b44904b23
Ruellia Zhang <syncyes@gmail.com>
2015-12-05 21:44:53 -0800

    images and diff file

---
+++ credits.js
@@ -0,0 +1,7 @@
+var Credits = {
+    create: function() {
+        var style = {font: '90px Brush Script MT', fill:'#660066', align: 'center'};
+        var styleWords = {font: '20px Sans Serif', fill:'#FFFFFF', align: 'center'};
+        this.credits = game.add.text(140,180, 'Credits', style);
+    }
+};
